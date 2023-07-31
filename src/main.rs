097594cccee718f1267103cc897c08927f8ccae1
churrundo <churrundo@gmail.com>
2023-07-31 17:31:24 -0600

    Implement GameState struct and Display trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct GameState {
     player1_board: Vec<u32>,
     player2_board: Vec<u32>,
@@ -6,12 +8,36 @@ struct GameState {
 }
 
 fn main() {
-    let mut game_state = GameState {
-        player1_board: vec![4, 4, 4, 4, 4, 4],  // 6 pits for player 1
-        player2_board: vec![4, 4, 4, 4, 4, 4],  // 6 pits for player 2
-        player1_score: 0,  // initial score for player 1
-        player2_score: 0,  // initial score for player 2
+    let game_state = GameState {
+        player1_board: vec![4, 4, 4, 4, 4, 4], // 6 pits for player 1
+        player2_board: vec![4, 4, 4, 4, 4, 4], // 6 pits for player 2
+        player1_score: 0, // initial score for player 1
+        player2_score: 0, // initial score for player 2
     };
 
-    println!("Hello, world!");
+    impl fmt::Display for GameState {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let player1_board = self.player1_board
+                .iter()
+                .map(ToString::to_string)
+                .collect::<Vec<String>>()
+                .join(" ");
+            let player2_board = self.player2_board
+                .iter()
+                .map(ToString::to_string)
+                .collect::<Vec<String>>()
+                .join(" ");
+
+            write!(
+                f,
+                "Player 2's Mancala:{}\nPlayer 2's board:{}\nPlayer 1's board:{}\nPlayer 1's Mancala:{}",
+                self.player2_score,
+                player2_board,
+                player1_board,
+                self.player1_score
+            )
+        }
+        
+    }
+    println!("{}", game_state);
 }
